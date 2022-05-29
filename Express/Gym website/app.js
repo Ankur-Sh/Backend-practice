f4ce7765948306a1c526ab459980e37917d1bc01
Ankur-Sh <ankursharma.as0403@gmail.com>
2022-05-29 14:23:55 +0530

    gym website backend completed using pug

--- Express/Gym website/app.js
+++ Express/Gym website/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 port = 1024;
 //EXPRESS SPECIFIC STUFF
 app.use("/static", express.static("static")); //For serving static files
+app.use(express.urlencoded());
 
 //PUG SPECIFIC STUFF
 app.set("view engine", "pug"); //set the template engine as pug
@@ -14,6 +16,17 @@ app.get("/", (req, res) => {
     const con =
         "this is the best content on the internet so far so use it wisely";
     const params = { title: "Pubg is the best Game", content: con };
+    res.status(200).render("index.pug", params);
+});
+app.post("/", (req, res) => {
+    name = req.body.name;
+    age = req.body.age;
+    gender = req.body.gender;
+    address = req.body.address;
+    more = req.body.more;
+    let outputToWrite = `THe name of the client is ${name}, ${age} years old, ${gender}, residing at ${address}. More about him/her: ${more}`;
+    fs.writeFileSync("output.txt", outputToWrite);
+    const params = { message: "Your form has been submitted succesfully" };
     res.status(200).render("index.pug", params);
 });
 
