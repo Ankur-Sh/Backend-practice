ba20e39a1914118b7229b6019af392623a13ff13
Ankur-Sh <ankursharma.as0403@gmail.com>
2022-06-15 01:34:06 +0530

    contact form backend completed using mongoose

--- Express/Dance Website/app.js
+++ Express/Dance Website/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const mongoose = require("mongoose");
+//const bodyparser = require("body-parser");
+mongoose.connect("mongodb://localhost/contactDance", { useNewUrlParser: true });
 port = 8000;
+
+//Define mongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String,
+});
+const Contact = mongoose.model("Contact", contactSchema);
 
 //EXPRESS SPECIFIC STUFF
 app.use("/static", express.static("static")); //For serving static files
@@ -18,7 +31,20 @@ app.get("/", (req, res) => {
 });
 app.get("/contact", (req, res) => {
     const params = {};
-    res.status(200).render("contact.pug", params);
+    res.status(200).render("contact.pug");
+});
+app.post("/contact", (req, res) => {
+    var myData = new Contact(req.body);
+    myData
+        .save()
+        .then(() => {
+            res.send("THis item has been saved to the database");
+        })
+        .catch(() => {
+            res.status(400).send("Item was not saved to the database");
+        });
+
+    // res.status(200).render("contact.pug", params);
 });
 
 //START THE SERVER
